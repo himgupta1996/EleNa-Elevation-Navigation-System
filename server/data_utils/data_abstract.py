725d2ad9299c9c9abddb8c2be777ea4ddc85ced4
Bhanu Bhandari <bhanubhandar@umass.edu>
2021-12-05 08:19:13 -0500

    connect data and algo abstractions

--- server/data_utils/data_abstract.py
+++ server/data_utils/data_abstract.py
@@ -2,16 +2,20 @@
 # from Elena.control.algorithms import Algorithms
 # from Elena.control.settings import *
 from geopy.geocoders import Photon
-
-init = False
-G, M, algorithms = None, None, None
+from server.graph_utils.graph_abstract import AbstractGraph
+from server.graph_utils.distance_calc import distance_calculate
 
 class DataAbstract(object):
     def __init__(self, logger):
         self.logger = logger
+        self.data = {}
+        self.geojson = {}
+        self.init = False
+        self.nx_graph = None
+        self.algorithms = None
 
     def initialize_data(self):
-        data = {
+        self.data = {
             "elevation_route": [],
             "shortest_route": [],
             "shortDist": 0,
@@ -24,43 +28,39 @@ class DataAbstract(object):
         }
 
     def get_geojson(self, coordinates):
-        geojson = {}
-        geojson["properties"] = {}
-        geojson["type"] = "Feature"
-        geojson["geometry"] = {}
-        geojson["geometry"]["type"] = "LineString"
-        geojson["geometry"]["coordinates"] = coordinates
-
-        return geojson
+        self.geojson["properties"] = {}
+        self.geojson["type"] = "Feature"
+        self.geojson["geometry"] = {}
+        self.geojson["geometry"]["type"] = "LineString"
+        self.geojson["geometry"]["coordinates"] = coordinates
+        return self.geojson
 
     def get_data_from_path(self, start, end, shortestPath, elevPath):
         if shortestPath is None and elevPath is None:
-            return data
-        data["start"] = start
-        data["end"] = end
-        data["elevation_route"] = self.get_geojson(elevPath[0])
-        data["shortest_route"] = self.get_geojson(shortestPath[0])
-        data["shortDist"] = shortestPath[1]
-        data["gainShort"] = shortestPath[2]
-        data["dropShort"] = shortestPath[3]
-        data["elenavDist"] = elevPath[1]
-        data["gainElenav"] = elevPath[2]
-        data["dropElenav"] = elevPath[3]
+            return self.data
+        self.data["start"] = start
+        self.data["end"] = end
+        self.data["elevation_route"] = self.get_geojson(elevPath[0])
+        self.data["shortest_route"] = self.get_geojson(shortestPath[0])
+        self.data["shortDist"] = shortestPath[1]
+        self.data["gainShort"] = shortestPath[2]
+        self.data["dropShort"] = shortestPath[3]
+        self.data["elenavDist"] = elevPath[1]
+        self.data["gainElenav"] = elevPath[2]
+        self.data["dropElenav"] = elevPath[3]
         if len(elevPath[0]) == 0:
-            data["popup_flag"] = 1
+            self.data["popup_flag"] = 1
         else:
-            data["popup_flag"] = 2
-        return data
+            self.data["popup_flag"] = 2
+        return self.data
 
     def get_data_point_from_location(self, locate, len_location):
         return locate[0] + ',' + locate[1] + ',' + locate[2] + ',' + locate[len_location - 5] + ',' + locate[
             len_location - 3] + ', USA - ' + locate[len_location - 2]
 
-    def get_data(self, startpt, endpt, x, min_max, log=True):
+    def get_data(self, startpt, endpt, elevation_ratio, min_max, log=True):
         # gets data for plotting the routes.
-        global init, G, M, algorithms
-
-        data = self.initialize_data()
+        self.initialize_data()
 
         locator = Photon(user_agent="myGeocoder")
         print("The start point is", startpt)
@@ -81,16 +81,13 @@ class DataAbstract(object):
         end = self.get_data_point_from_location(locate, len_location)
         if log:
             print("End: ", end)
-
         if log:
             print("Percent of Total path: ", x)
             print("Elevation: ", min_max)
-        if not init:
-            abstract = Graph_Abstraction()
-            G = abstract.get_graph(endpt)
-            algorithms = Algorithms(G, x=x, elev_type=min_max)
-            init = True
-
-        shortestPath, elevPath = algorithms.get_shortest_path(startpt, endpt, x, elev_type=min_max, log=log)
-
+        if not self.init:
+            abstract = AbstractGraph()
+            self.nx_graph = abstract.get_graph(endpt)
+            algorithms = distance_calculate(self.nx_graph, elevation_adjust=elevation_ratio, elevation_type=min_max)
+            self.init = True
+        shortestPath, elevPath = algorithms.get_shortest_path(startpt, endpt, elevation_ratio, elevation_type=min_max, log=log)
         return self.get_data_from_path(start, end, shortestPath, elevPath)
